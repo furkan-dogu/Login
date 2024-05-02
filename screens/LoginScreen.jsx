474dd5a89dbc5eeb7e6d5923eadc3279b012c775
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-02 18:15:09 +0300

    firebase kuruldu

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,63 +1,96 @@
-import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import { auth } from "../firebase";
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Kullanıcı ", user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
-    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Email' style={styles.input} />
-        <TextInput placeholder='Şifre' style={styles.input} />
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          placeholder="Şifre"
+          style={styles.input}
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Giriş Yap</Text>
+          <Text style={styles.buttonText}>Giriş Yap</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.outlineButton]}>
-            <Text style={styles.outlineButtonText}>Kayıt Ol</Text>
+        <TouchableOpacity style={[styles.button, styles.outlineButton]} onPress={handleSignUp}>
+          <Text style={styles.outlineButtonText}>
+            Kayıt Ol
+          </Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    inputContainer: {
-        width: "80%"
-    },
-    input: {
-        backgroundColor: "white",
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        marginTop: 5,
-        borderRadius: 10
-    },
-    buttonContainer: {
-        width: "60%",
-        marginTop: 40
-    },
-    button: {
-        backgroundColor: "#0782F9",
-        padding: 15,
-        alignItems: "center",
-        borderRadius: 10
-    },
-    buttonText: {
-        color: "white",
-        fontSize: 16,
-        fontWeight: "700"
-    },
-    outlineButton: {
-        backgroundColor: "white",
-        marginTop: 5
-    },
-    outlineButtonText: {
-        color: "#0782F9",
-        fontSize: 16,
-        fontWeight: "700"
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginTop: 5,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    width: "60%",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    padding: 15,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  outlineButton: {
+    backgroundColor: "white",
+    marginTop: 5,
+  },
+  outlineButtonText: {
+    color: "#0782F9",
+    fontSize: 16,
+    fontWeight: "700",
+  },
+});
