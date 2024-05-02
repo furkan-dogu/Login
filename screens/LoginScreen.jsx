4d2d2129ea83dc3eb6a13e12b06745ce2e376927
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-02 13:19:28 +0300

    css yapıldı

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,12 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Email' style={styles.input} />
+        <TextInput placeholder='Şifre' style={styles.input} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Giriş Yap</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.outlineButton]}>
+            <Text style={styles.outlineButtonText}>Kayıt Ol</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    inputContainer: {
+        width: "80%"
+    },
+    input: {
+        backgroundColor: "white",
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        marginTop: 5,
+        borderRadius: 10
+    },
+    buttonContainer: {
+        width: "60%",
+        marginTop: 40
+    },
+    button: {
+        backgroundColor: "#0782F9",
+        padding: 15,
+        alignItems: "center",
+        borderRadius: 10
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "700"
+    },
+    outlineButton: {
+        backgroundColor: "white",
+        marginTop: 5
+    },
+    outlineButtonText: {
+        color: "#0782F9",
+        fontSize: 16,
+        fontWeight: "700"
+    }
+})
