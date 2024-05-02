3134ed3b14c9330faac6f1d49f594aa809fc28aa
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-02 19:08:16 +0300

    bitti

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -6,22 +6,45 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth } from "../firebase";
+import { useNavigation } from "@react-navigation/native";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if(user) {
+        navigation.navigate("Home")
+      }
+    })
+  }, [])
+  
 
   const handleSignUp = () => {
     auth
       .createUserWithEmailAndPassword(email, password)
       .then((userCredentials) => {
         const user = userCredentials.user;
-        console.log("Kullanıcı ", user.email);
+        console.log("Kullanıcı", user.email);
       })
       .catch((error) => alert(error.message));
   };
+
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Kullanıcı giriş yaptı", user.email);
+      })
+      .catch((error) => alert(error.message));
+  }
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
@@ -40,7 +63,7 @@ export default function LoginScreen() {
         />
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
           <Text style={styles.buttonText}>Giriş Yap</Text>
         </TouchableOpacity>
         <TouchableOpacity style={[styles.button, styles.outlineButton]} onPress={handleSignUp}>
